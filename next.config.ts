fca3d9f22232b69ed4c5d3476b55e834ef21f91f
Essential-Infotech <33011566+mohsin895@users.noreply.github.com>
2026-01-06 22:48:40 +0600

    eslint update

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http', 
-        hostname: '204.197.173.249',
-        port: '8014', 
-        pathname: '/storage/**', 
-      },
-    ],
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 
-    domains: [
-      "peru-stork-399467.hostingersite.com", 
-      "kashasears.codexwizardssquad.com"    
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "204.197.173.249",
+                port: "8014",
+                pathname: "/storage/**",
+            },
+        ],
+        domains: [
+            "peru-stork-399467.hostingersite.com",
+            "kashasears.codexwizardssquad.com",
+        ],
+    },
 };
 
 export default nextConfig;
